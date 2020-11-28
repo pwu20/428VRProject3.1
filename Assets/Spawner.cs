58262fbf498c46843f404dba17ee1fd6c4aaa308
Peter Wu <59982014+pwu20@users.noreply.github.com>
2020-11-27 22:17:16 -0600

    project 3.1 finished

---
+++ Assets/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+
+{	
+	public GameObject spawn;
+	public Transform startPosRot;
+	public Rigidbody ballBody;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+      spawn.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		if (spawn.activeSelf){
+			
+			Rigidbody ballSpawn;
+			ballSpawn = Instantiate(ballBody, startPosRot.position, startPosRot.rotation);
+			ballSpawn.AddForce(startPosRot.forward * 5000f);
+
+			spawn.SetActive(false);
+
+		}
+			
+
+    }
+
+}
